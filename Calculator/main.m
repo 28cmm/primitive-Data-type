e15dd127ac1812ba7516b09f31a773b97c8965a0
28cmm <joshuafang1998@gmail.com>
2018-12-23 12:46:08 -0800

    Initial Commit

---
+++ Calculator/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by Yilei Huang on 2018-12-23.
+//  Copyright © 2018 Joshua Fanng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
